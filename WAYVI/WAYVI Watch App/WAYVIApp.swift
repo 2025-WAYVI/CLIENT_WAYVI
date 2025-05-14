dfaf0be9f5aa3aaf5eaffac2f7c0d2864c452fd9
sunhanmoo <ghdms1013@naver.com>
2025-05-14 13:42:15 +0900

    프로젝트 세팅

---
+++ WAYVI/WAYVI Watch App/WAYVIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WAYVIApp.swift
+//  WAYVI Watch App
+//
+//  Created by 성호은 on 5/14/25.
+//
+
+import SwiftUI
+
+@main
+struct WAYVI_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
