977e2b56a7c68f3f69c4eca38a3be2fedf309c02
Lee Jihee <129031670+munjji@users.noreply.github.com>
2025-05-30 14:53:45 +0900

    🔨 Refactor: MainView 배포 버전으로 수정

--- WAYVI/WAYVI Watch App/Views/MainView.swift
+++ WAYVI/WAYVI Watch App/Views/MainView.swift
@@ -5,16 +5,11 @@
 //  Created by 이지희 on 5/28/25.
 //
 
+
 import SwiftUI
 
 struct MainView: View {
     @AppStorage("isLoggedIn") private var isLoggedIn = false
-
-    init() {
-        #if DEBUG
-        UserDefaults.standard.set(false, forKey: "isLoggedIn")
-        #endif
-    }
 
     var body: some View {
         if isLoggedIn {
@@ -24,18 +19,3 @@ struct MainView: View {
         }
     }
 }
-
-// TODO: 서버 배포 후, 해당 코드로 수정
-//import SwiftUI
-//
-//struct MainView: View {
-//    @AppStorage("isLoggedIn") private var isLoggedIn = false
-//
-//    var body: some View {
-//        if isLoggedIn {
-//            HomeNavigationView()
-//        } else {
-//            LoginView()
-//        }
-//    }
-//}
